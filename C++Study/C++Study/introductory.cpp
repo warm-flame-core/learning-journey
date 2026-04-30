4c735d2c7b09b2a13af207cf8c857c0548b5919b
warm-flame-core <1994768512@qq.com>
2026-04-30 20:24:16 +0800

    今天开始C++的学习

---
+++ C++Study/C++Study/introductory.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////命名空间
+//namespace wfc
+//{
+//	int rand = 10;
+//	int Add(int a, int b)
+//	{
+//		return a + b;
+//	}
+//	
+//	/*namespace xie
+//	{
+//		int rand = 50;
+//	}*/
+//
+//}
+//using wfc::rand;
+//int main()
+//{
+//	printf("%d\n", rand);
+//	printf("%d\n", wfc::Add(1, 2));
+//	/*printf("%d\n", wfc::xie::rand);*/
+//	return 0;
+//}
+
+
+//输入和输出
+#include <iostream>
+int main()
+{
+	int a = 0;
+	double b = 1.11;
+	char c = 'x';
+	
+	return 0;
+}
