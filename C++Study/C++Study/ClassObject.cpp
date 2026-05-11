3a38fef27233c6ee61a075ff7f9883ba42a05f36
warm-flame-core <1994768512@qq.com>
2026-05-11 21:30:12 +0800

    新增析构函数和拷贝构造函数

--- C++Study/C++Study/ClassObject.cpp
+++ C++Study/C++Study/ClassObject.cpp
@@ -1,14 +1,172 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 using namespace std;
+//class Date
+//{
+//public:
+//	void Init(int year = 10, int mouth = 10, int day = 10)
+//	{
+//		this->_mouth = mouth;
+//		this->_year = year;
+//		this->_day = day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << " " << _mouth << " " << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _mouth;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;
+//	d1.Init();
+//	d1.Print();
+//	return 0;
+//}
+
+
+//class Date
+//{
+//public:
+//	Date()
+//	{
+//		_year = 1;
+//		_mouth = 1;
+//		_day = 1;
+//	}
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_mouth = month;
+//		_day = day;
+//	}
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_mouth = month;
+//		_day = day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << " " << _mouth << " " << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _mouth;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1(2026,5,11);
+//	d1.Print();
+//	return 0;
+//}
+
+
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack(int n = 4)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败");
+//				return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//};
+//
+////内置类型构造时不确定，自定义类型会看自定义类型的构造
+//class MyQueue
+//{
+//public:
+//	//编译器默认?成MyQueue的构造函数调用了Stack的构造，完成了两个成员的初始化
+//private:
+//	Stack pushst;
+//	Stack popst;
+//};
+
+//析构函数
+//typedef int STDataType;
+//class Stack
+//{
+//public:
+//	Stack(int n = 4)
+//	{
+//		_a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		if (nullptr == _a)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		_capacity = n;
+//		_top = 0;
+//	}
+//	~Stack()
+//	{
+//		cout << "~Stack" << endl;
+//		free(_a);
+//		_a = nullptr;
+//		_top = _capacity = 0;
+//	}
+//private:
+//	STDataType* _a;
+//	size_t _capacity;
+//	size_t _top;
+//};
+//
+////内置类型构造时不确定，自定义类型会看自定义类型的构造
+//class MyQueue
+//{
+//public:
+//	//编译器默认生成MyQueue的构造函数调?了Stack的构造，完成了两个成员的初始化
+//	//编译器默认生成MyQueue的析构函数调用了Stack的析构，销毁了两个成员
+//private:
+//	Stack pushst;
+//	Stack popst;
+//};
+//
+//int main()
+//{
+//	MyQueue m;
+//	
+//	return 0;
+//}
+
+
+//拷贝构造函数
 class Date
 {
 public:
-	void Init(int year = 10, int mouth = 10, int day = 10)
+	Date(int year = 1, int month = 1, int day = 1)
 	{
-		this->_mouth = mouth;
-		this->_year = year;
-		this->_day = day;
+		_year = year;
+		_mouth = month;
+		_day = day;
+	}
+	Date(const Date& d)//函数默认还有一个this指针，不显式写
+	{
+		_year = d._year;
+		_mouth = d._mouth;
+		_day = d._day;
+	}
+	Date(Date* d)//不是拷贝构造，就是普通的构造函数
+	{
+		_year = d->_year;
+		_mouth = d->_mouth;
+		_day = d->_day;
 	}
 	void Print()
 	{
@@ -19,10 +177,14 @@ private:
 	int _mouth;
 	int _day;
 };
+
 int main()
 {
-	Date d1;
-	d1.Init();
+	Date d1(2026,5,11);
 	d1.Print();
+	Date d2(d1);
+	d2.Print();
+	Date d3(&d1);
+	d3.Print();
 	return 0;
 }
