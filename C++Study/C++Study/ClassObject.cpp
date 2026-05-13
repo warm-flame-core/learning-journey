db67e398dcc3e3beb673b8fda6dbfc3212ec91a7
warm-flame-core <1994768512@qq.com>
2026-05-13 22:15:37 +0800

    新增类和对象中日期类的实现

--- C++Study/C++Study/ClassObject.cpp
+++ C++Study/C++Study/ClassObject.cpp
@@ -1,6 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <iostream>
-using namespace std;
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
 //class Date
 //{
 //public:
@@ -147,44 +147,101 @@ using namespace std;
 
 
 //拷贝构造函数
-class Date
-{
-public:
-	Date(int year = 1, int month = 1, int day = 1)
-	{
-		_year = year;
-		_mouth = month;
-		_day = day;
-	}
-	Date(const Date& d)//函数默认还有一个this指针，不显式写
-	{
-		_year = d._year;
-		_mouth = d._mouth;
-		_day = d._day;
-	}
-	Date(Date* d)//不是拷贝构造，就是普通的构造函数
-	{
-		_year = d->_year;
-		_mouth = d->_mouth;
-		_day = d->_day;
-	}
-	void Print()
-	{
-		cout << _year << " " << _mouth << " " << _day << endl;
-	}
-private:
-	int _year;
-	int _mouth;
-	int _day;
-};
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_mouth = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)//函数默认还有一个this指针，不显式写，且参数应该是引用，防止无穷递归
+//	//自定义类型传值传参会调用拷贝构造
+//	{
+//		_year = d._year;
+//		_mouth = d._mouth;
+//		_day = d._day;
+//	}
+//	Date(Date* d)//不是拷贝构造，就是普通的构造函数
+//	{
+//		_year = d->_year;
+//		_mouth = d->_mouth;
+//		_day = d->_day;
+//	}
+//	void Print()
+//	{
+//		cout << _year << " " << _mouth << " " << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _mouth;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1(2026,5,11);
+//	d1.Print();
+//	Date d2(d1);
+//	d2.Print();
+//	Date d3(&d1);
+//	d3.Print();
+//	return 0;
+//}
 
-int main()
-{
-	Date d1(2026,5,11);
-	d1.Print();
-	Date d2(d1);
-	d2.Print();
-	Date d3(&d1);
-	d3.Print();
-	return 0;
-}
+//#include <stdbool.h>
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_mouth = month;
+//		_day = day;
+//	}
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_mouth = d._mouth;
+//		_day = d._day;
+//	}
+//
+//	bool operator==(Date& d2)
+//	{
+//		return _year == d2._year
+//			&& _mouth == d2._mouth
+//			&& _day == d2._day;
+//	}
+//
+//	bool operator!=(Date& d2)
+//	{
+//		return !(*this == d2);
+//	}
+//	
+//	void Print()
+//	{
+//		cout << _year << "-" << _mouth << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _mouth;
+//	int _day;
+//};
+
+//bool operator==(const Date& d1, Date& d2)
+//{
+//	return d1._year == d2._year
+//		&& d1._mouth == d2._mouth
+//		&& d1._day == d2._day;
+//}
+
+//int main()
+//{
+//	Date d1(2026, 5, 13);
+//	Date d2 = d1;
+//	d1 == d2;
+//	d1.Print();
+//	d2.Print();
+//	return 0;
+//}
