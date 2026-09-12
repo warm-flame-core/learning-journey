6e0c6bb8b4caf3bbe1cc3ad5b1eb467abdc1600a
warm-flame-core <1994768512@qq.com>
2026-09-12 19:50:54 +0800

    Linux学习环境变量

---
+++ linux/environment_variable/code.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[], char *env[])
+{
+    (void)argc;
+    (void)argv;
+    (void)env;
+
+   // char *value = getenv("PATH");
+   // if(value == NULL)
+   //     return 1;
+   // printf("PATH->%s\n", value);
+
+
+   // for(int i = 0; env[i]; i++)
+   // {
+   //     printf("env[%d]-> %s\n", i, env[i]);
+   // }
+
+
+   // if(argc != 2)
+   // {
+   //     printf("Usage: %s [-a|-b|-c]\n", argv[0]);
+   //     return 1;
+   // }
+
+
+   // const char *arg = argv[1];
+
+   // if(strcmp(arg, "-a") == 0)
+   //     printf("功能1\n");
+
+   // else if(strcmp(arg, "-b") == 0)
+   //     printf("功能2\n");
+
+   // else if(strcmp(arg, "-c") == 0)
+   //     printf("功能3\n");
+
+   // else if(strcmp(arg, "-d") == 0)
+   //     printf("功能4\n");
+   // else
+   //     printf("Usage: %s [-a|-b|-c]\n", argv[0]);
+
+
+
+   // for(int i = 0; i < argc ; i++)
+   // {
+   //     printf("argv[%d]: %s\n", i, argv[i]);
+   // }
+    return 0;
+}
