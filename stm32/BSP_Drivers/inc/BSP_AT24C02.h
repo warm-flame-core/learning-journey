8d2693d109be375472624311464fa14520c5cc20
warm-flame-core <1994768512@qq.com>
2026-09-15 22:33:35 +0800

    新增EEPROM学习

---
+++ stm32/BSP_Drivers/inc/BSP_AT24C02.h
@@ -0,0 +1,15 @@
+#ifndef __BSP_AT24C02_H
+#define __BSP_AT24C02_H
+
+#ifdef __cpluscplus
+extern "C"
+{
+#endif
+
+
+    
+#ifdef __cpluscplus
+}
+#endif
+
+#endif
---
+++ stm32/BSP_Drivers/src/BSP_AT24C02.c
@@ -0,0 +1,1 @@
+#include "BSP_AT24C02.h"
