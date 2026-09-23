2988cec74ec522f39dda2e58f0ffef4abd18af2b
warm-flame-core <1994768512@qq.com>
2026-09-23 16:18:12 +0800

    开始unordered_set和unordered_map封装，先搭框架

---
+++ data structure/my_unordered_map_my_unordered_set/my_unordered_map_my_unordered_set/Hash.h
@@ -0,0 +1,241 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include <string>
+using namespace std;
+
+// 仿照stl库设置素数表
+inline unsigned long __stl_next_prime(unsigned long n)
+{
+	// Note: assumes long is at least 32 bits.
+	static const int __stl_num_primes = 28;
+	static const unsigned long __stl_prime_list[__stl_num_primes] = {
+		53, 97, 193, 389, 769,
+		1543, 3079, 6151, 12289, 24593,
+		49157, 98317, 196613, 393241, 786433,
+		1572869, 3145739, 6291469, 12582917, 25165843,
+		50331653, 100663319, 201326611, 402653189, 805306457,
+		1610612741, 3221225473, 4294967291
+	};
+	const unsigned long* first = __stl_prime_list;
+	const unsigned long* last = __stl_prime_list + __stl_num_primes;
+	const unsigned long* pos = lower_bound(first, last, n);
+	return pos == last ? *(last - 1) : *pos;
+}
+
+
+template<class K>
+struct HashFunc
+{
+	size_t operator()(const K& key)
+	{
+		return (size_t)key;
+	}
+};
+
+// 模板偏特化，对于常见比较类型使用偏特化可以少传一个参数
+template<>
+struct HashFunc<string>
+{
+	size_t operator()(const string& s)
+	{
+		// BKDR算法，字符串哈希函数
+		size_t hash = 0;
+		for (auto ch : s)
+		{
+			hash += ch;
+			hash *= 131;
+		}
+
+		return hash;
+	}
+};
+
+
+
+
+namespace xjw
+{
+	template<class K, class V>
+	struct HashNode
+	{
+		pair<K, V> _kv;
+		HashNode<K, V>* _next;
+
+		HashNode(const pair<K, V>& kv)
+			:_kv(kv)
+			, _next(nullptr)
+		{
+		}
+	};
+
+	template<class K, class V, class Hash = HashFunc<K>>
+	class HashTable
+	{
+		typedef HashNode<K, V> Node;
+	public:
+		HashTable()
+			:_tables(__stl_next_prime(0))
+			//:_tables(11)
+			, _n(0)
+		{
+		}
+
+		~HashTable()
+		{
+			for (int i = 0;i < _tables.size();i++)
+			{
+				Node* cur = _tables[i];
+				Node* next = nullptr;
+				while (cur)
+				{
+					next = cur->_next;
+					delete cur;
+					cur = next;
+				}
+				_tables[i] = nullptr;
+			}
+			_n = 0;
+		}
+
+		HashTable(const HashTable<K, V, Hash>& ht)
+			:_tables(ht._tables.size())
+			,_n(ht._n)
+		{
+			for (int i = 0;i < ht._tables.size();i++)
+			{
+				Node* cur = ht._tables[i];
+				while (cur)
+				{
+					Insert(cur->_kv);
+					cur = cur->_next;
+				}
+			}
+		}
+
+		HashTable<K, V, Hash>& operator=(HashTable<K, V, Hash> ht)
+		{
+			swap(_tables, ht._tables);
+			swap(_n, ht._n);
+			return *this;
+		}
+
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			// 不允许冗余
+			if (Find(kv.first))
+				return false;
+
+
+
+			Hash hash;
+			// 扩容，链地址法大于1扩容
+			if (_n > _tables.size())
+			{
+
+
+				// 这样写效率很低，因为现在vector是挂节点，复用插入逻辑是先销毁再创建节点，效率低下
+				/*HashTable<K, V> newht;
+				newht._tables.resize(__stl_next_prime(_tables.size() + 1));
+				for (int i = 0;i < _tables.size();i++)
+				{
+					Node* cur = _tables[i];
+					while (cur)
+					{
+						newht.Insert(cur);
+						cur = cur->_next;
+					}
+				}
+				_tables.swap(newht._tables);
+				_n = newht._n;*/
+
+
+				//直接把节点拿下来会更好
+				//vector<Node*> newTables((_tables.size() * 2));	//测试的时候换2倍很好测试
+
+				
+				
+				vector<Node*> newTables(__stl_next_prime(_tables.size() + 1));
+				for (int i = 0;i < _tables.size();i++)
+				{
+					Node* cur = _tables[i];
+					while (cur)
+					{
+						Node* next = cur->_next;
+						size_t hashi = hash(cur->_kv.first) % newTables.size();
+						// 头插
+						cur->_next = newTables[hashi];
+						newTables[hashi] = cur;
+						cur = next;
+					}
+					_tables[i] = nullptr;
+				}
+				_tables.swap(newTables);
+			}
+
+
+
+			size_t hashi = hash(kv.first) % _tables.size();
+			// 头插
+			Node* newnode = new Node(kv);
+			newnode->_next = _tables[hashi];
+			_tables[hashi] = newnode;
+			++_n;
+			return true;
+		}
+
+
+		Node* Find(const K& key)
+		{
+			Hash hash;
+			size_t hashi = hash(key) % _tables.size();
+			Node* cur = _tables[hashi];
+			while (cur)
+			{
+				if (cur->_kv.first == key)
+					return cur;
+				cur = cur->_next;
+			}
+			return nullptr;
+		}
+
+
+		bool Erase(const K& key)
+		{
+			Hash hash;
+			size_t hashi = hash(key) % _tables.size();
+			Node* prev = nullptr;
+			Node* cur = _tables[hashi];
+			while (cur)
+			{
+				if (cur->_kv.first == key)
+				{
+					// 头节点
+					if (_tables[hashi] == cur)
+					{
+						_tables[hashi] = cur->_next;
+					}
+					// 中间节点
+					else
+					{
+						prev->_next = cur->_next;
+					}
+					delete cur;
+					--_n;
+					return true;
+				}
+
+				//继续往下找
+				prev = cur;
+				cur = cur->_next;
+
+			}
+			return false;
+		}
+
+	private:
+		vector<Node*> _tables;
+		size_t _n;
+	};
+}
